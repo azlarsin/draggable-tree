21fd9be09230199ae7c8e5764943503c2a42d45f
azlar <azlarsin@gmail.com>
2017-09-25 14:45:16 +0800

    add readme & todo list

---
+++ README.md
@@ -0,0 +1,18 @@
+# draggable-tree
+A draggable tree by pure javascript.
+
+## How to use
+
+
+## Api
+
+
+## demo
+
+## todo
+- [x] 逻辑编写、打包
+- [ ] 事件编写
+- [ ] api
+- [ ] readme
+- [ ] 发布
+- [ ] demo
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@
  * Created by azlar on 16/04/2017.
  */
 
-const path = require('path');
+const path = require('path'),
+    webpack = require('webpack'),
+    isProduction = process.env.NODE_ENV === 'production',
+    filename = isProduction ? "DraggableTree.min.js" : "DraggableTree.js";
 
 module.exports = {
     entry: [
@@ -10,7 +13,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, "lib"),
-        filename: 'DraggableTree.js',
+        filename: filename,
         library: "DraggableTree"
     },
 
@@ -18,9 +21,28 @@ module.exports = {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets:['es2015', 'stage-0']} },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
-        ],
+        ]
     },
-    externals: {
-        'react': 'commonjs react'
-    }
+
+    plugins: isProduction ?
+        [
+            new webpack.optimize.UglifyJsPlugin({
+                minimize: true,
+                comments: false,
+                sourceMap: true,
+                compress: {
+                    unused: true,
+                    dead_code: true, // big one--strip code that will never execute
+                    warnings: false, // good for prod apps so users can't peek behind curtain
+                    drop_debugger: true,
+                    conditionals: true,
+                    evaluate: true,
+                    drop_console: true, // strips console statements
+                    sequences: true,
+                    booleans: true,
+                }
+            })
+        ]
+        :
+        []
 };
